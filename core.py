7032d38f2adbee5fd38939647b597c7dfc6f8688
fxzjshm <fxzjshm@163.com>
2017-08-29 18:25:31 +0800

    Add ui

--- core.py
+++ core.py
@@ -1,8 +1,13 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_script import Manager
 
-import ocr
-
+try:
+    import Image
+except ImportError:
+    from PIL import Image
+import pytesseract
+import base64
+import cStringIO
 
 app = Flask(__name__)
 manager = Manager(app)
@@ -10,6 +15,15 @@ manager = Manager(app)
 @app.route('/')
 def test():
     return 'Hello but I will make you tired ...'
+
+@app.route('/ocr', methods=['POST'])
+def ocr():
+    return pytesseract.image_to_string(Image.open(cStringIO.StringIO(base64.b64decode(request.data))))
+
+@app.route('/ocr', methods=['GET'])
+def ocrUI():
+    return render_template('ocr.html')
+
 
 if __name__ == "__main__":
     manager.run()
